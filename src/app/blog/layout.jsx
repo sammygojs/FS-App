003847348ad5e53d37023f81e4c08be2686e4331
Sumit <akoliyasumit@hotmail.com>
2024-07-01 17:05:02 +0100

    blog layout

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BlogLayout({children}) {
+  return (
+    <div>
+        <h2>This is the blog layout</h2>
+        {children}
+    </div>
+  )
+}
