12b44a64b9fbecef17255f7c0c6c58a05ed64fc2
Sumit <akoliyasumit@hotmail.com>
2024-07-01 17:32:30 +0100

    error, loading and not found pages

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h2>Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link href="/">Return Home</Link>
+    </div>
+  )
+}
