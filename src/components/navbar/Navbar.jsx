1c471a699f6f1dbbb02b244b167054657b528d60
Sumit <akoliyasumit@hotmail.com>
2024-07-01 17:21:32 +0100

    Adding links to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Link from "next/link"
+
+import Links from "./links/Links"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <div>Logo</div>
+        <div>
+            <Links/>
+            {/* <Link href="/">Homepage</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link> */}
+        </div>
+    </div>
   )
 }
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from "next/link"
+
+export default function Links() {
+    const links = [
+        {
+            title:"Homepage",
+            path: "/",
+        },
+        {
+            title:"About",
+            path: "/about",
+        },
+        {
+            title:"Contact",
+            path: "/contact",
+        },
+        {
+            title:"Blog",
+            path: "/blog",
+        },
+    ];
+
+  return (
+    <div>
+        {links.map((link=>(
+            <Link href={link.path} key={link.title}>{link.title}</Link>
+        )))}
+    </div>
+  )
+}
